6dd08fb762ea7f7c42fcabedae1debb1c90e4b3a
Bonsaiiv <dominik.riemer@udo.edu>
2023-12-06 16:48:48 +0100

    funny edit

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -5,16 +5,23 @@ fn main() {
     let input_str = fs::read_to_string("resources/input").unwrap();
     let mut lines = input_str.lines();
     let re_nums = Regex::new(r"(\d+)").unwrap();
-    let mut inputs = re_nums.captures_iter(&lines.next().unwrap())
+    let result1: usize = re_nums.captures_iter(&lines.next().unwrap())
         .map(|cap| cap.extract())
         .map(|(_,[time_str])| time_str.parse::<usize>().unwrap())
         .zip(re_nums.captures_iter(lines.next().unwrap())
         .map(|cap| cap.extract())
-        .map(|(_,[time_str])| time_str.parse::<usize>().unwrap()));
-    let mut result1 = 1;
-    for (time, goal_distance) in inputs{
-        result1 *= (0 .. time).into_iter().map(|t_hold| t_hold * (time-t_hold)).filter(|dist| dist > &goal_distance).count(); 
-    }
+        .map(|(_,[time_str])| time_str.parse::<usize>().unwrap()))
+        .map(|(time, goal_distance)| 
+            (0 .. time)
+                .into_iter()
+                .map(|t_hold| t_hold * (time-t_hold))
+                .filter(|dist| dist > &goal_distance)
+                .count())
+                .product();
+    //let mut result1 = 1;
+    //for (time, goal_distance) in inputs{
+        //result1 *= (0 .. time).into_iter().map(|t_hold| t_hold * (time-t_hold)).filter(|dist| dist > &goal_distance).count(); 
+    //}
     println!("{result1}");
 
     lines = input_str.lines();
