304e474640bd51002fc085de5c75475268fd605a
Bonsaiiv <dominik.riemer@udo.edu>
2024-12-03 06:11:58 +0100

    finish day02

--- 2024/src/day02.c
+++ 2024/src/day02.c
@@ -49,7 +49,7 @@ int try_skip(int* a, size_t len, size_t index, int is_ascending) {
 	next_num = a[0];
 	size_t i = 1;
 	if (index == 0){
-		next_num = a[0];
+		next_num = a[1];
 		i = 2;
 	}
 	for (; i < len; i++){
@@ -58,14 +58,12 @@ int try_skip(int* a, size_t len, size_t index, int is_ascending) {
 		next_num = a[i];
 		//printf("last num: %d, next_num: %d\n", last_num, next_num);
 		int diff = next_num - last_num;
-		if (!is_ascending) {
-			diff = -diff;
-		}
+		diff = diff*is_ascending;
 		if (diff < 1 || diff > 3) {
 			return 0;
 		}
 	}
-	printf("successfully skiped index %d\n", (int)index);
+	//printf("successfully skiped index %d\n", (int)index);
 	return 1;
 }
 int get_input_line(FILE** fp, int** ret_a){
@@ -102,45 +100,14 @@ void print_arr(int* a, size_t len) {
 void day02part2(char* filename){
 	int res = 0;
 	int len;
-	int last_num, next_num, is_ascending, success;
+	int success;
 	FILE* fp = fopen(filename, "r");
 	int* a;
-	int line_nr = 0;
 	while ((len = get_input_line(&fp, &a)) != -1) {
-		line_nr++;
-		success = 1;
-		is_ascending = 1;
-		if (a[0] > a[1]) is_ascending = 0;
-		if (a[0] == a[1]){
-			printf("\nfailing because same on line %d\n", line_nr);
-			print_arr(a, len);
-			if (try_skip(a, len, 1, 0) || try_skip(a, len, 1, 1))
-				res++;
-			continue;
-		}
-		if (abs(a[0] - a[1]) > 3) {
-			printf("\nfailing because distance on line %d\n", line_nr);
-			print_arr(a, len);
-			if(try_skip(a, len, 1, 0) || try_skip(a, len, 0, 0) || try_skip(a, len, 1, 1) || try_skip(a, len, 0, 1))
-				res++;
-			continue;
-		}
-		//printf("ascending: %d\n", is_ascending);
-		for (int i = 1; i < len; i++){
-			last_num = a[i-1];
-			next_num = a[i];
-			//printf("last num: %d, next_num: %d\n", last_num, next_num);
-			int diff = next_num - last_num;
-			if (!is_ascending) {
-				diff = -diff;
-			}
-			if (diff < 1 || diff > 3) {
-				printf("\nfailing between %d ,%d on line %d\n", i-1, i, line_nr);
-				print_arr(a, len);
-				success = try_skip(a, len, i-1, is_ascending) || try_skip(a, len, i, is_ascending);
-				if (i == 2 && !success) success = try_skip(a, len, 0, !is_ascending);
-				break;
-			}
+		success = 0;
+		for (int i = 0; i <= len && !success; i++) {
+			success += try_skip(a, len, i, -1);
+			success += try_skip(a, len, i, 1);
 		}
 		if (success) res++;
 		free(a);
