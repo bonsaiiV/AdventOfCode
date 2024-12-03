9989d35892a9b5222c19982a6036c68165193e44
Bonsaiiv <dominik.riemer@udo.edu>
2024-12-03 06:51:54 +0100

    solve day 03

--- 2024/include/days.h
+++ 2024/include/days.h
@@ -3,6 +3,8 @@ void day01part1(char input[]);
 void day01part2(char input[]);
 void day02part1(char input[]);
 void day02part2(char input[]);
+void day03part1(char input[]);
+void day03part2(char input[]);
 
 
 struct __attribute__((packed)) day {
@@ -13,4 +15,5 @@ struct __attribute__((packed)) day {
 struct day daymap[] = {
 	{day01part1, day01part2},
 	{day02part1, day02part2},
+	{day03part1, day03part2},
 };
---
+++ 2024/src/day03.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static char* digits = "0123456789";
+int get_digit(int* ret, char in){
+	for (int i = 0; i < 10; i++) {
+		if (digits[i] == in) {
+			*ret = i;
+			return 1;
+		}
+	}
+	return 0;
+}
+void day03part1(char* filename){
+	int res = 0, line_nr = 1;
+	int digit, a, b;
+	char* line = calloc(80, sizeof(char));
+	size_t i, line_len = 80;
+	FILE* fp = fopen(filename, "r");
+	while (getline(&line, &line_len, fp) != -1) {
+		//printf("line: %d\n", line_nr);
+		i = 0;
+		while (line[i] != 0){
+			a = 0;
+			b = 0;
+			if (!strncmp("mul(", line + i, 4)) {
+				//printf("detected mul at index %ld\n", i);
+				i += 4;
+				while(get_digit(&digit, line[i])){ 
+					a = a * 10 + digit;
+					i++;
+				}
+				//printf("a is %d at %ld\n", a, i);
+				if (a == 0 || line[i] != ','){
+					continue;
+				}
+				i++;
+				while(get_digit(&digit, line[i])){ 
+					b = b * 10 + digit;
+					i++;
+				}
+				if (b == 0 || line[i] != ')'){
+					continue;
+				}
+				//printf("b is %d\n", a);
+				res += a*b;
+			}
+			i++;
+		}
+	}
+	free(line);
+	printf("result: %d\n", res);
+}
+void day03part2(char* input){
+	int res = 0, line_nr = 1;
+	int digit, a, b;
+	int enabled = 1;
+	char* line = calloc(80, sizeof(char));
+	size_t i, line_len = 80;
+	FILE* fp = fopen(filename, "r");
+	while (getline(&line, &line_len, fp) != -1) {
+		//printf("line: %d\n", line_nr);
+		i = 0;
+		while (line[i] != 0){
+			a = 0;
+			b = 0;
+			if (!strncmp("do()", line + i, 4)) {
+				i += 4;
+				enabled = 1;
+			}
+			if (!strncmp("don't()", line + i, 6)) {
+				i += 4;
+				enabled = 0;
+			}
+			if (enabled && !strncmp("mul(", line + i, 4)) {
+				//printf("detected mul at index %ld\n", i);
+				i += 4;
+				while(get_digit(&digit, line[i])){ 
+					a = a * 10 + digit;
+					i++;
+				}
+				//printf("a is %d at %ld\n", a, i);
+				if (a == 0 || line[i] != ','){
+					continue;
+				}
+				i++;
+				while(get_digit(&digit, line[i])){ 
+					b = b * 10 + digit;
+					i++;
+				}
+				if (b == 0 || line[i] != ')'){
+					continue;
+				}
+				//printf("b is %d\n", a);
+				res += a*b;
+			}
+			i++;
+		}
+	}
+	free(line);
+	printf("result: %d\n", res);
+}
