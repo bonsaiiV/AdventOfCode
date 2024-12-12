37e38ed9eaeb013e7f195cfd4cff068a4e87ed3f
Bonsaiiv <dominik.riemer@udo.edu>
2024-12-12 20:34:10 +0100

    implement hashmap for day11

--- 2024/src/day11.c
+++ 2024/src/day11.c
@@ -51,7 +51,7 @@ void day11part1(char* filename){
 	DBG("getting input\n");
 	get_input(fp, &nums);
 	fclose(fp);
-	for (i = 0; i < 75; i++) {
+	for (i = 0; i < 25; i++) {
 		for (j = 0; j < nums.len; j++) {
 			DBG("%ld, ", nums.data[j]);
 			apply_rules(nums.data[j], &next_nums);
@@ -81,10 +81,92 @@ void stone_list_add(stone_list* list, stone a) {
 	}
 	stone_list_insert(list, a);
 }
-void day11part2(char* filename){
+#define bucket_count 101
+size_t stone_hash(stone in, size_t max) {
+	return in.val % max;
+}
+struct hash_map {
+	stone_list buckets[bucket_count];
+};
+struct hash_map stone_map_create(void){
+	struct hash_map ret;
+	size_t i;
+	for (i = 0; i < bucket_count; i++) {
+		ret.buckets[i] = stone_list_create();
+	}
+	return ret;
+}
+void stone_map_add(struct hash_map* map, stone in) {
+	stone_list_add(map->buckets + stone_hash(in, bucket_count), in);
+}
+size_t stone_max_bucket(struct hash_map* map) {
+	size_t i;
+	size_t len, max = 0;
+	for (i = 0; i < bucket_count; i++) {
+		len = map->buckets[i].len;
+		if (len > max) max = len;
+	}
+	return max;
+}
+size_t stone_map_count(struct hash_map* map) {
+	size_t i;
+	size_t count = 0;
+	for (i = 0; i < bucket_count; i++) {
+		count += map->buckets[i].len;
+	}
+	return count;
+}
+size_t stone_map_sum(struct hash_map* map) {
 	size_t i, j;
-	stone_list stones = stone_list_create();
-	stone_list tmp, next_stones = stone_list_create();
+	size_t sum = 0;
+	for (i = 0; i < bucket_count; i++) {
+		for (j = 0; j < map->buckets[i].len; j++) {
+			sum += map->buckets[i].data[j].count;
+		}
+	}
+	return sum;
+}
+int map_get_next(struct hash_map* in_map, stone* ret) {
+	static struct hash_map* map = 0;
+	if (in_map) {
+		map = in_map;
+		return 1;
+	}
+	if (!map) return -1;
+	if (!ret) return -1;
+	static size_t next_bucket = 0;
+	static size_t next_index = 0;
+	while (1) {
+		if (next_bucket >= bucket_count) {
+			next_bucket = 0;
+			next_index = 0;
+			return 2;
+		}
+		if (next_index < map->buckets[next_bucket].len) {
+			*ret = map->buckets[next_bucket].data[next_index++];
+			return 0;
+		} else {
+			next_index = 0;
+			next_bucket++;
+		}
+	} 
+}
+void map_empty(struct hash_map* map) {
+	size_t i;
+	for (i = 0; i < bucket_count; i++) {
+		map->buckets[i].len = 0;
+	}
+}
+void map_clean(struct hash_map* map) {
+	size_t i;
+	for (i = 0; i < bucket_count; i++) {
+		free(map->buckets[i].data);
+	}
+}
+void day11part2(char* filename){
+	size_t i;
+	struct hash_map stones = stone_map_create();
+	struct hash_map tmp, next_stones = stone_map_create();
 	stone helper, a;
 	FILE* fp = fopen(filename, "r");
 	DBG("getting input\n");
@@ -97,18 +179,18 @@ void day11part2(char* filename){
 	tok = strtok(line, " ");
 	do {
 		helper.val = atol(tok); helper.count = 1;
-		stone_list_add(&stones, helper);
+		stone_map_add(&stones, helper);
 		line = calloc(last_line_len, sizeof(char));
 	} while ((tok = strtok(0, " ")));
 	free(line);
 	fclose(fp);
 	for (i = 0; i < 75; i++) {
-		for (j = 0; j < stones.len; j++) {
-			a = stones.data[j];
+		map_get_next(&stones, 0);
+		while (!map_get_next(0, &a)) {
 			DBG("{%ld, %ld}, ", a.val, a.count);
 			if (a.val == 0) {
 				a.val = 1;
-				stone_list_add(&next_stones, a);
+				stone_map_add(&next_stones, a);
 				continue;
 			}
 			int digits = 0;
@@ -125,24 +207,21 @@ void day11part2(char* filename){
 				helper.val = a.val / div;
 				helper.count = a.count;
 				a.val -= helper.val * div;
-				stone_list_add(&next_stones, helper);
-				stone_list_insert(&next_stones, a);
+				stone_map_add(&next_stones, helper);
+				stone_map_add(&next_stones, a);
 			} else {
 				a.val *= 2024;
-				stone_list_insert(&next_stones, a);
+				stone_map_add(&next_stones, a);
 			}
 		}
 		DBG("\n");
 		tmp = stones;
 		stones = next_stones;
 		next_stones = tmp;
-		next_stones.len = 0;
+		map_empty(&next_stones);
 	}
-	long long res = 0;
-	for (i = 0; i < stones.len; i++) {
-		res += stones.data[i].count;
-	}
+	printf("max bucket: %ld\n", stone_max_bucket(&stones));
+	long long res = stone_map_sum(&stones);
 	printf("result: %lld\n", res);
-	free(stones.data);
-	free(next_stones.data);
+	map_clean(&stones);
 }
