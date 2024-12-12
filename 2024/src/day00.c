07c7e9c8afa6cb0b3d6dc7f75a12bc5d0b3240a4
Bonsaiiv <dominik.riemer@udo.edu>
2024-12-12 05:55:28 +0100

    template day00 uses macros for lists

--- 2024/src/day00.c
+++ 2024/src/day00.c
@@ -3,45 +3,26 @@
 #include <string.h>
 #include "globals.h"
 
-struct list {
-	char** data;
-	size_t cap;
-	size_t len;
-};
-static struct list list_create(void) {
-	char** data = calloc(8, sizeof(char*));
-	struct list ret = {data, 8, 0};
-	return ret;
-}
-static void list_insert(struct list* list, char* elem) {
-	if (list->cap == list->len) {
-		list->cap *= 2;
-		list->data = reallocarray(list->data, list->cap, sizeof(char*));
-	}
-	list->data[list->len++] = elem;
-}
-static void list_clean(struct list* list) {
-	for (size_t i = 0; i < list->len; i++) {
-		free(list->data[i]);
-	}
-	list->len = 0;
-}
 
-static void get_input(FILE* fp, struct list* list) {
+typedef char* string;
+LIST(string)
+LIST_CLEAN(string)
+
+static void get_input(FILE* fp, string_list* list) {
 	size_t last_line_len = 8;
 	char* line = calloc(last_line_len, sizeof(char));
 	while (getline(&line, &last_line_len, fp) != -1) {
 		if (!strcmp(line, "\n")){
 			break;
 		}
-		list_insert(list, line);
+		string_list_insert(list, line);
 		line = calloc(last_line_len, sizeof(char));
 	}
 	free(line);
 }
 void dayXXpart1(char* filename){
 	size_t i;
-	struct list lines = list_create();
+	string_list lines = string_list_create();
 	FILE* fp = fopen(filename, "r");
 	int res = 0;
 	DBG("getting input\n");
@@ -50,20 +31,9 @@ void dayXXpart1(char* filename){
 	}
 	fclose(fp);
 	printf("result: %d\n", res);
-	list_clean(&lines);
+	string_list_clean(&lines);
 	free(lines.data);
 }
 void dayXXpart2(char* filename){
-	size_t i;
-	struct list lines = list_create();
-	FILE* fp = fopen(filename, "r");
-	int res = 0;
-	DBG("getting rules\n");
-	get_input(fp, &lines);
-	for (i = 0; i < lines.len; i++) {
-	}
-	fclose(fp);
-	printf("result: %d\n", res);
-	list_clean(&lines);
-	free(lines.data);
+	(void) filename;
 }
